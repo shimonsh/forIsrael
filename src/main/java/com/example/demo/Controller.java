3683fa49bcfb6a98f34eabcfed37e6b8d445629a
shimontheshalev <shimontheshalev@gmail.com>
2021-02-16 18:48:44 +0200

    hi

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,13 +3,14 @@ package com.example.demo;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
 public class Controller {
@@ -17,16 +18,33 @@ public class Controller {
     List<String> ips = new ArrayList<>();
     private final String israelPass = "israel";
 
+    @GetMapping(
+            value = "/logo",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody
+    byte[] getImageWithMediaType(HttpServletRequest request) throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/taiwan.jpg");
+        saveIp(request);
+        return in.readAllBytes();
+    }
+
     @GetMapping(path = "/print")
     public String getCallerAddress(HttpServletRequest request) {
+        saveIp(request);
+        return "Hello world!";
+    }
+
+    private void saveIp(HttpServletRequest request) {
         String returnValue;
         if(request.getHeader("X-Forwarded-For") != null){
             returnValue = request.getHeader("X-Forwarded-For");
         }else{
             returnValue = request.getRemoteAddr();
         }
-        ips.add(returnValue);
-        return "Hello world!";
+        Date date = new Date();
+        ips.add(date.toString() + " : " + returnValue);
     }
 
     @GetMapping(path="/getFile/{pass}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
